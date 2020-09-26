5fbe0d63b87a4bb0e9727aa94fce6d58c0fd7c4f
Josephine Zaina Brandstrup Weirsøe <s164177@win.dtu.dk>
2020-09-26 09:58:20 +0200

    expandble view start

--- app/src/main/java/com/example/dormcuments/ui/meeting/MeetingFragment.kt
+++ app/src/main/java/com/example/dormcuments/ui/meeting/MeetingFragment.kt
@@ -5,32 +5,33 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.doOnAttach
 import androidx.fragment.app.Fragment
 import com.example.dormcuments.R
-import com.example.dormcuments.ui.shopping.AddShopItem
-import com.example.dormcuments.ui.shopping.Item
-import com.example.dormcuments.ui.shopping.adapter_shop
-import com.example.dormcuments.ui.shopping.item_
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_meeting.*
-import java.util.ArrayList
+import java.util.*
 
 class MeetingFragment : Fragment() {
     var meetArr: ArrayList<Topic> = ArrayList()
     var database = FirebaseDatabase.getInstance().getReference("Agenda")
     val meet = topic_
+    lateinit var titleLayout: ConstraintLayout
+    lateinit var sumLayout: ConstraintLayout
+    lateinit var expand: ImageView
     lateinit var getdata : ValueEventListener;
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_meeting, container, false)
+        var root = inflater.inflate(R.layout.fragment_meeting, container, false)
 
         meetArr.clear()
 
@@ -70,6 +71,9 @@ class MeetingFragment : Fragment() {
 
 
 
+
+        makeList()
+
         root.findViewById<FloatingActionButton>(R.id.add).setOnClickListener {
             requireFragmentManager().beginTransaction().add(
                 R.id.nav_host_fragment,
@@ -83,5 +87,40 @@ class MeetingFragment : Fragment() {
         super.onDetach()
         database.removeEventListener(getdata)
 
-    };
+    }
+
+    private fun makeList(){
+
+        //Inflater to XML filer ind, et Cardview og en Spacer som bruges til at skabe afstand fordi det ikke er muligt med Padding eller Layout Margin.
+        val ExpandableCardview: View =
+            layoutInflater.inflate(R.layout.list_element_meeting, null, false)
+
+        sumLayout = ExpandableCardview.findViewById(R.id.sumLayout)
+        titleLayout = ExpandableCardview.findViewById(R.id.titleLayout)
+        expand = ExpandableCardview.findViewById(R.id.expand)
+
+        //Set OnClickListener that handles expansion and collapse of view
+        titleLayout.setOnClickListener {
+            expandList(sumLayout, expand)
+        }
+
+        expand.setOnClickListener {
+            expandList(sumLayout, expand)
+        }
+
+    }
+
+    private fun expandList(
+        sumLayout: ConstraintLayout,
+        expand: ImageView
+    ) {
+        if (sumLayout.visibility == View.GONE) {
+            sumLayout.visibility = View.VISIBLE
+            expand.rotation = 90f
+        } else if (sumLayout.visibility == View.VISIBLE) {
+            sumLayout.visibility = View.GONE
+            expand.rotation = 0f
+        }
+    }
+
 }
