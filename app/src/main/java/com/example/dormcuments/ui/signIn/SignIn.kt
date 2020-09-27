ec7e99c7a35577686d39a73a1476775fba992b70
Josephine Zaina Brandstrup Weirsøe <s164177@win.dtu.dk>
2020-09-27 23:47:56 +0200

    signin icons color

--- app/src/main/java/com/example/dormcuments/ui/signIn/SignIn.kt
+++ app/src/main/java/com/example/dormcuments/ui/signIn/SignIn.kt
@@ -1,18 +1,39 @@
 package com.example.dormcuments.ui.signIn
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dormcuments.MainActivity
 import com.example.dormcuments.R
-import com.example.dormcuments.ui.calender.CalenderFragment
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
+
 class SignIn : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        mail.setOnTouchListener { v, event ->
+            if (MotionEvent.ACTION_UP == event.action) {
+                mail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.email_icon_tint, 0, 0, 0)
+                password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.password_icon, 0, 0, 0)
+            }
+            false
+        }
+
+        password.setOnTouchListener { v, event ->
+            if (MotionEvent.ACTION_UP == event.action) {
+                mail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.email_icon, 0, 0, 0)
+                password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.password_icon_tint, 0, 0, 0)
+            }
+            false
+        }
+
 
 
         signIpButton.setOnClickListener(View.OnClickListener {
@@ -29,5 +50,10 @@ class SignIn : AppCompatActivity() {
             val intent = Intent(applicationContext, ForgottenPassword::class.java)
             startActivity(intent)
         })
+    }
+
+    private fun resetDrawable(){
+        mail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.email_icon, 0, 0, 0)
+        password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.password_icon, 0, 0, 0)
     }
 }
