3f282a3e8ac11d674b25c192706804c93bcf5369
Josephine Zaina Brandstrup Weirsøe <s164177@win.dtu.dk>
2020-11-12 11:56:17 +0100

    crashalytics

--- app/src/main/java/com/joeSoFine/dormcuments/MainActivity.kt
+++ app/src/main/java/com/joeSoFine/dormcuments/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.joeSoFine.dormcuments
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +27,6 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
 
+        firebaseAnalytics = Firebase.analytics
     }
-
 }
