9f278554c6fd3eba56f8877756a63f9b108fb5ca
Josephine Zaina Brandstrup Weirsøe <s164177@win.dtu.dk>
2021-01-10 22:06:56 +0100

    progressbar

--- app/src/main/java/com/joeSoFine/dormcuments/ui/meeting/SummaryMenuFragment.kt
+++ app/src/main/java/com/joeSoFine/dormcuments/ui/meeting/SummaryMenuFragment.kt
@@ -5,10 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.joeSoFine.dormcuments.R
 import com.joeSoFine.dormcuments.UITools
@@ -28,6 +25,9 @@ class SummaryMenuFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_summary_menu, container, false)
         val list = root.findViewById<ListView>(R.id.list)
         val myWebView: WebView = root.findViewById(R.id.webs)
+        val progressBar: ProgressBar = root.findViewById(R.id.progressBar12)
+        progressBar.visibility = View.VISIBLE
+
 
         root.findViewById<ImageView>(R.id.question).setOnClickListener{
             UITools.onHelpedClicked(requireContext(),R.string.helpDialogTitleSum, R.string.helpDialogMsgSum)
@@ -61,6 +61,7 @@ class SummaryMenuFragment : Fragment() {
                     myWebView.loadUrl(sumArr[position].url)
 
                 }
+                progressBar.visibility = View.GONE
 
             })
 
