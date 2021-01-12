53397ddcf656eb9db21480d036cb8210a5065df5
Josephine Zaina Brandstrup Weirsøe <s164177@win.dtu.dk>
2021-01-12 20:36:54 +0100

    dialog fix

--- app/src/main/java/com/joeSoFine/dormcuments/ui/signIn/SignIn.kt
+++ app/src/main/java/com/joeSoFine/dormcuments/ui/signIn/SignIn.kt
@@ -51,6 +51,7 @@ class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        animation_view.visibility = View.GONE
 
         auth = Firebase.auth
 
