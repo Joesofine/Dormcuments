a0d5b9f7ae05e2e17c6c63df8976c7465f0475c5
Josephine Zaina Brandstrup Weirsøe <s164177@win.dtu.dk>
2021-01-16 16:16:50 +0100

    rule fix

--- app/src/main/java/com/joeSoFine/dormcuments/ui/rules/RulesFragment.kt
+++ app/src/main/java/com/joeSoFine/dormcuments/ui/rules/RulesFragment.kt
@@ -43,16 +43,40 @@ class  RulesFragment : Fragment() {
             withContext(Dispatchers.Main){
 
                 for (i: Int in 0..csv.size-1) {
-                    val rule = csv[i].replace("\"", "")//.split(",")[0]
-                    ruleArr.add(rule)
-                    lottie.visibility = View.GONE
+                    val rule = csv[i].split(",")
+                    var rules = ""
+                    var ruleSub = ""
+                    var ruleSub1 = ""
+                    var ruleSub2 = ""
 
+                    for (i in 0..rule.size - 1){
+                            if (i != rule.size - 1) {
+                                rules = "$rules, ${rule[i]}"
+                            }
+                    }
+                    if (rules.substring(0,1) == ","){
+                        ruleSub = rules.substring(2,rules.length)
+                    } else {
+                        ruleSub = rules
+                    }
+                    if (ruleSub.substring(0,1) == "\""){
+                        ruleSub1 = ruleSub.substring(1,ruleSub.length)
+                    } else {
+                        ruleSub1 = ruleSub
+                    }
+                    if (ruleSub1.substring(ruleSub1.length-1,ruleSub1.length) == "\""){
+                        ruleSub2 = ruleSub1.substring(0,ruleSub1.length-1)
+                    } else {
+                        ruleSub2 = ruleSub1
+                    }
+                    ruleArr.add(ruleSub2)
                 }
 
                 if (ruleArr.size != 0) {
                     val adaptor = context?.let { adapter_rules(it, ruleArr) }
                     root.findViewById<ListView>(R.id.list).adapter = adaptor
                 }
+                lottie.visibility = View.GONE
             }
         }
         return root
